84445ec306e13cc8cf963ad5dee0ad41180333b9
Matti Kjellstadli <mattikjellstadli@icloud.com>
2025-03-20 23:11:34 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<script setup>
+
+import Brands from "@/components/Brands.vue";
+</script>
+
+<template>
+  <Brands
+      image="https://yt3.googleusercontent.com/QvzqDJOtfThI4QVIqw3qORtKEmcE3zd97PCxOAVG0NMdWegygXFbUm7pPmSSfmC8SRH6Ue5Fpw=s160-c-k-c0x00ffffff-no-rj"
+      :links="['https://shelstad.itch.io/', 'https://www.youtube.com/channel/UCCffG6dAJjgbZbw2p0KgVBQ']"
+      name="Shelstad Studios"
+      description="Personal Game Development Projects"
+  />
+  <Brands
+      image="https://yt3.googleusercontent.com/IxJunmeSafWr3Y81_N_g1CZVHUuY6AOAHSOx2AFuetArLsSVojwksdHV2kyqkFzan5Kc_DhvDA=s160-c-k-c0x00ffffff-no-rj"
+      :links="['https://www.youtube.com/@Triggas', 'https://github.com/mattkje']"
+      name="TRGS"
+      description="Personal Web Development Projects"
+/>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
---
+++ src/components/Brands.vue
@@ -0,0 +1,71 @@
+<script setup>
+import { defineProps } from 'vue';
+
+const props = defineProps({
+  name: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  image: {
+    type: String,
+    required: true
+  },
+  links: {
+    type: Array,
+    required: true
+  }
+});
+</script>
+
+<template>
+  <div class="container">
+    <img class="brand-logo" :src="props.image" alt="LOGO"/>
+    <h1>{{ props.name }}</h1>
+    <p>{{ props.description }}</p>
+    <ul>
+      <li v-for="(link, index) in props.links" :key="index">
+        <a :href="link">{{ link }}</a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  text-align: center;
+  margin: 20px;
+}
+
+.brand-logo {
+  max-width: 100px;
+  border-radius: 1.5rem;
+  margin-bottom: 10px;
+}
+
+h1 {
+  font-size: 1.5em;
+  margin-bottom: 10px;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  margin: 5px 0;
+}
+
+a {
+  color: #007BFF;
+  text-decoration: none;
+}
+
+a:hover {
+  text-decoration: underline;
+}
+</style>
